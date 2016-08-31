fb1c72625ab4e7df18507de8a23bb12299a27035
Eric Liu <eric@loveagency.com.au>
2016-08-31 18:09:45 +1000

    adds flavor to library

--- app/src/main/java/com/example/ericliu/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ericliu/myapplication/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.ericliu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvMain = (TextView) findViewById(R.id.tvMain);
+
+        tvMain.setText("The Magic Number is: ");
     }
 }
---
+++ mylibrary/src/prod/java/com.example.mylibrary/Injection.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+/**
+ * Created by ericliu on 31/08/16.
+ */
+public class Injection {
+
+    int provideInteger() {
+        return 100;
+    }
+}
